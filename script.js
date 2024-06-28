b60de573259877831050eaff5e2594e29f0520a4
Irene <gallibour.irene@gmail.com>
2024-06-28 14:33:55 +0200

    test2 non concluant

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('')
+fetch('https://whatyearisit-backend-kappa-five.vercel.app/year')
 .then(response => response.json())
 .then( data => 
     document.querySelector('#year').textContent = data.year
